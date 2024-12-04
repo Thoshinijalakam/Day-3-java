0511c3b02e9aff4a55246c8351f7cec815d6e458
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-04 13:30:27 +0530

    Add files via upload

---
+++ Power2.java
@@ -0,0 +1,12 @@
+public class Power2 {
+    public static void main(String [] args){
+
+    int a=8388608;
+    if((a&(a-1))==0){
+        System.out.println("power of 2");
+
+    }else{
+    System.out.println("not a power of 2");
+    }
+}
+}
