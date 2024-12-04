fa2a56b034a38081b9a5bfaadd743adfed1f10cd
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-04 15:13:23 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Ifelse{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=10;
+        if((a&1)==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+   }
+}
+
